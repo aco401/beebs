188e32284c4322bb8aea8c17efbcf1e76c349efe
aco401 <tobias.westphal@haw-hamburg.de>
2022-05-09 20:22:18 +0200

    src/select: Fix multiple defintions+

--- src/select/libselect.c
+++ src/select/libselect.c
@@ -35,7 +35,7 @@ do{               \
     (b)=temp;     \
 }while(0);        \
 
-float arr[20] = {
+float select_arr[20] = {
   5, 4, 10.3, 1.1, 5.7, 100, 231, 111, 49.5, 99,
   10, 150, 222.22, 101, 77, 44, 35, 20.54, 99.99, 888.88
 };
@@ -53,42 +53,42 @@ float select(unsigned long k, unsigned long n)
 	while (!flag) {
 		if (ir <= l+1) {
 			if (ir == l+1)
-			  if (arr[ir] < arr[l]) {
-			    SWAP(arr[l],arr[ir])
+			  if (select_arr[ir] < select_arr[l]) {
+			    SWAP(select_arr[l],select_arr[ir])
 			      }
 			flag = 1;
 		} else if (!flag) {
 			mid=(l+ir) >> 1;
-			SWAP(arr[mid],arr[l+1])
-			if (arr[l+1] > arr[ir]) {
-				SWAP(arr[l+1],arr[ir])
+			SWAP(select_arr[mid],select_arr[l+1])
+			if (select_arr[l+1] > select_arr[ir]) {
+				SWAP(select_arr[l+1],select_arr[ir])
 			}
-			if (arr[l] > arr[ir]) {
-				SWAP(arr[l],arr[ir])
+			if (select_arr[l] > select_arr[ir]) {
+				SWAP(select_arr[l],select_arr[ir])
 			}
-			if (arr[l+1]> arr[l]) {
-				SWAP(arr[l+1],arr[l])
+			if (select_arr[l+1]> select_arr[l]) {
+				SWAP(select_arr[l+1],select_arr[l])
 			}
 			i=l+1;
 			j=ir;
-			a=arr[l];
+			a=select_arr[l];
 			while (!flag2) {
 				i++;
-				while (arr[i] < a) i++;
+				while (select_arr[i] < a) i++;
 				j--;
-				while (arr[j] > a) j--;
+				while (select_arr[j] > a) j--;
 				if (j < i) flag2 = 1;
-				if (!flag2) SWAP(arr[i],arr[j]);
+				if (!flag2) SWAP(select_arr[i],select_arr[j]);
 
 			}
-			arr[l]=arr[j];
-			arr[j]=a;
+			select_arr[l]=select_arr[j];
+			select_arr[j]=a;
 			if (j >= k) ir=j-1;
 			if (j <= k) l=i;
 		}
 
 	}
-	return arr[k];
+	return select_arr[k];
 }
 
 static int x, y;
